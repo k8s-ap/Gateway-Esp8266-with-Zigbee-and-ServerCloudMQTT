370a669ae6a6f551c82cb7b46f2aaf7a0ee58cd1
Adrian <perez.adrian.matias@gmail.com>
2020-03-06 23:06:34 -0300

    Se seto el tiempo de sincronizacion para que sea cada 1 hora.

--- IoSamplesXbee-MQTT-wemosD1Mini.ino
+++ IoSamplesXbee-MQTT-wemosD1Mini.ino
@@ -1,5 +1,5 @@
 /*
- * rama fusion/timestamp
+ * Rama fusion/timestamp
 Basic ESP8266 MQTT example
 
  This sketch demonstrates the capabilities of the pubsub library in combination
@@ -32,26 +32,61 @@ The XBee coordinator should be connected to the Wemos-D1-Mini.
 This example uses the SoftSerial library to view the XBee communication.  I am using a 
 Modern Device USB BUB board (http://moderndevice.com/connect) and viewing the output
 with the Arduino Serial Monitor.
-*/
-#include <XBee.h>
-#include <SoftwareSerial.h>
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
 
 // Connect Wemos-d1-mini pin Rx to TX of Coordinator-XbeeS2C device
 // Connect Wemos-d1-mini pin Tx to RX of Coordinator-XbeeS2C device
 // Remember to connect all devices to a common Ground: Coordinator-XbeeS2C, Wemos-d1-mini and TTL-USB-Serial device
 
+*/
+#include <XBee.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h> // How we connect to your local wifi
+#include <PubSubClient.h>
+#include <WiFiUdp.h> // UDP library which is how we communicate with Time Server
+#include <TimeLib.h> // See Arduino Playground for details of this useful time synchronisation library
+
 // Update these with values suitable for your network.
-const char* ssid = "TNTsport";
-const char* password = "9122018TresUno";
+//const char* ssid = "TNTsport";
+//const char* password = "9122018TresUno";
 const char* mqtt_server = "mqtt.diveriot.com";
 const int mqtt_port = 1883;
+#define _ssid "TNTsport"
+#define _password "9122018TresUno"
 
+//------------BEGIN Declaracion feature timestamp--------------//
+#define WifiTimeOutSeconds 10
+unsigned int localPort = 8888; // Just an open port we can use for the UDP packets coming back in
+
+// This is the "pool" name for any number of NTP servers in the pool.
+// If you're not in the UK, use "time.nist.gov"
+// Elsewhere: USA us.pool.ntp.org
+// Read more here: http://www.pool.ntp.org/en/use.html
+char timeServer[] = "time.nist.gov"; //"uk.pool.ntp.org";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+WiFiUDP Udp; // A UDP instance to let us send and receive packets over UDP
+const int timeZone = -3; // Your time zone relative to GMT/UTC. // Not used (yet)
+String DoW[] = {"Domingo", "Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"}; // Days of week. Day 1 = Sunday
+
+// How often to resync the time (under normal and error conditions)
+//#define _resyncSeconds 60
+#define _resyncSeconds 3600 //300 is 5 minutos // 3600 is 1 hour. 86400 is on day
+#define _resyncErrorSeconds 15
+#define _millisMinute 60000
+
+// forward declarations
+void connectToWifi();
+void printDigits(int digits);
+void digitalClockDisplay();
+time_t getNTPTime();
+
+//------------FIN Declaracion feature timestamp--------------//
+
+
+//------------BEGIN Declaracion feature mqtt and Xbee-Arduino--------------//
 WiFiClient espClient;
 PubSubClient client(espClient);
 //long lastMsg = 0;
-char msg[50];
 char gas[50];
 char motion[50];
 char door[50];
@@ -64,88 +99,52 @@ XBeeAddress64 endDeviceXBeeAddress = XBeeAddress64(0x0013A200, 0x4180A081);
 float nodeHighAddress; // para identificar quien de quien es el frame, del EndDevice o del Router
 float nodeLowAddress;
 
-void setup_wifi() {
-  //luego reemplazar esta funcion por otra que utiliza la libreria wifi-multi.h
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial1.println();
-  Serial1.print("Connecting to ");
-  Serial1.println(ssid);
 
-  WiFi.begin(ssid, password);
+//------------FIN Declaracion feature mqtt and Xbee-Arduino--------------//
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial1.print(".");
-  }
 
-  randomSeed(micros());//genera un numero aletaorio que parte del parametro micros() que devuelve el tiempo(hora:mm:ss:mls) en el que inicio el programa en la placa wemos-d1-mini
-
-  Serial1.println("");
-  Serial1.println("WiFi connected");
-  Serial1.println("IP address: ");
-  Serial1.println(WiFi.localIP());
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-  //El WemosD1mini posee esta logica para estar atento si hay nuevas publicaciones en el topic suscrito (casa/living/luz) y en caso afirmativo
-  //enviar un frame al Xbee-Router para que activar/desactivar Relee(foco)
-  
-  // Switch on the LED if an 1 was received as first character//envia un frame al XBee-Router(XBee-terminal) con "1" para activar el Relee que enciende el foco de luz
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-}
-
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial1.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ClienteWemosD1Mini-";
-    clientId += String(random(0xffff), HEX);//cada vez que nos reconectamos, debemos hacerlo con un nuevo clientID (no el mismo).
-    // Attempt to connect
-    if (client.connect(clientId.c_str())) { //c_str() Convierte el contenido de una cadena a una cadena de estilo C. Nunca debe modificar la cadena a través del puntero devuelto.
-      Serial1.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("Log", "Dispositivo ESP8266ClientHome reconectado al servidor mqtt://mqtt.diveriot.com:1883");
-      // ... and resubscribe
-      client.subscribe("Casa/LivingRoom/Luz"); // Esto creo que esta de más? O me debo suscribir a tal TOPIC cuando me conecto por primera vez al serverMQTT?
-    } else {
-      Serial1.print("failed, rc=");
-      Serial1.print(client.state());
-      Serial1.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
+//-----------------------------------------------------------------------------
+// SETUP     SETUP     SETUP     SETUP     SETUP     SETUP     SETUP     SETUP
+//-----------------------------------------------------------------------------
 
 void setup() { 
+  //-------config de propositos compartidos entre todas las bibliotecas ---------//
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output.  //LED del Wemos D1. Para otra placa borrar esta variable con toda la funcione que implique
   Serial.begin(9600);           // Inicio comunicacion-serial con el XBeeS2C-Coordinador
   xbee.setSerial(Serial);     // Seteo/vinculo dicha conmunicacion-serial con el objeto xbee.
   // start soft serial
-  Serial1.begin(9600);        // Inicio comunicacion-serial-1 para mostrar por pantalla los mensajes de depuración.
+  Serial1.begin(9600);        // Inicio comunicacion-serial-1 para mostrar por pantalla los mensajes de depuración. ¿Y espero a que se abra el puerto declarado al inicio?
   WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
-  setup_wifi();
+  //setup_wifi();  // ya no uso este primer metodo para conectarme a la red.  Luego eliminar
+  connectToWifi();  // Connect to your local wifi (one time operation)
+
+  //-------config timestamp---------//
+  Udp.begin(localPort); // What port will the UDP/NTP packet respond on?
+  setSyncProvider(getNTPTime); // What is the function that gets the time (in ms since 01/01/1900)?
+  // How often should we synchronise the time on this machine (in seconds)?
+  // Use 300 for 5 minutes but once an hour (3600) is more than enough usually
+  // Use 86400 for 1 daybut once an hour (3600) is more than enough usually
+  setSyncInterval(_resyncSeconds); // just for demo purposes!
+  //setSyncInterval(300); // se sincronizara cada 5 minutos
+
+  //-------config mqtt---------//
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 }
 
+
+//-----------------------------------------------------------------------------
+// LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP     LOOP
+//-----------------------------------------------------------------------------
 void loop() {
-  //attempt to read a packet (intento leer un frame)    
+  //-------logica timestamp---------//
+  
+  digitalClockDisplay(); // This just prints the "system time"  
+  delay(0.1 * _millisMinute); // mins * millis in 1 min // Do other stuff etc, delay here just emulates that
+
+  
+  //-------logica mqtt---------//
+  //attempt to read a packet (intento leer un frame)   
   xbee.readPacket();
 
   if (xbee.getResponse().isAvailable()) {
@@ -275,10 +274,10 @@ void loop() {
                    * ésta será recortada, es decir, si reservamos 50 bytes para la cadena, con sprintf() tal vez se escriban más, 
                    * depende de los datos a escribir, pero snprintf() escribirá 50.
                   */
-                  snprintf (msg, 50, "{\"value\":\"Danger\", \"timestamp\":\"22:13:00\"}");
+                  snprintf (gas, 50, "{\"value\":\"Danger\", \"timestamp\":\"22:13:00\"}");
                   Serial1.print("Publish message topic 'Casa/Cocina/Gas': ");
-                  Serial1.println(msg);
-                  client.publish("Casa/Cocina/Gas", msg);
+                  Serial1.println(gas);
+                  client.publish("Casa/Cocina/Gas", gas);
                   /**/        
                 }
                 //Serial1.print("Digital (DI");
@@ -327,5 +326,228 @@ void loop() {
   } else if (xbee.getResponse().isError()) {
     Serial1.print("Error reading packet.  Error code: ");  
     Serial1.println(xbee.getResponse().getErrorCode());
+  }
+}
+
+//-----------------------------------------------------------------------------
+// DECLARATIONS METHODS AND FUNCTIONS   -   DECLARATIONS METHODS AND FUNCTIONS
+//-----------------------------------------------------------------------------
+
+
+// Prints a nice time display
+//-----------------------------------------------------------------------------
+void digitalClockDisplay() {
+  // We'll grab the time so it doesn't change whilst we're printing it
+  time_t t=now();
+
+  //Now print all the elements of the time secure that it won't change under our feet
+  //printDigits(hour(t)-3); // le sumo tres unidades para obtgener la hora correspondiente a mi zona (timezone=-3)
+  printDigits(hour(t));
+  Serial1.print(":");
+  printDigits(minute(t));
+  Serial1.print(":");
+  printDigits(second(t));
+  Serial1.print("    ");
+  Serial1.print(DoW[weekday(t)-1]);
+  Serial1.print(" ");
+  printDigits(day(t));
+  Serial1.print("/");
+  printDigits(month(t));
+  Serial1.print("/");
+  printDigits(year(t));
+  Serial1.println();
+}
+
+void printDigits(int digits) {
+  // utility for digital clock display: prints leading 0
+  if (digits < 10) Serial1.print('0');
+  Serial1.print(digits);
+}
+
+
+// This is the function to contact the NTP pool and retrieve the time
+//-----------------------------------------------------------------------------
+time_t getNTPTime() {
+  // Send a UDP packet to the NTP pool address
+  Serial1.print("\nSending NTP packet to "); 
+  Serial1.println(timeServer);
+  sendNTPpacket(timeServer);  // Enviamos un paquete al servidor NTP para pedir los segundos desde 1900 y sincronizar
+
+  // Wait to see if a reply is available - timeout after X seconds. At least
+  // this way we exit the 'delay' as soon as we have a UDP packet to process
+  #define UDPtimeoutSecs 5
+  int timeOutCnt = 0;
+  while (Udp.parsePacket() == 0 && ++timeOutCnt < (UDPtimeoutSecs * 10)){
+    delay(100);
+    yield(); //Pasa el control a otras tareas cuando se llama. Idealmente, yield () debería usarse en funciones que tardarán un tiempo en completarse
+  }
+
+  // Is there UDP data present to be processed? Sneak a peek!
+  if (Udp.peek() != -1) {
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    // The time-stamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, extract the two words:
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900)
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial1.print("Seconds since Jan 1 1900 = ");
+    Serial1.println(secsSince1900);
+ 
+    // now convert NTP time into everyday time:
+    //Serial1.print("Unix time = ");
+
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+
+    // subtract seventy years and convert to time zone local:
+    unsigned long epoch = secsSince1900 - seventyYears + timeZone * SECS_PER_HOUR;
+
+    // Reset the interval to get the time from NTP server in case we previously changed it. Por ejemplo en caso de error del servidor que no responde y utilizo el _resyncErrorSeconds
+    setSyncInterval(_resyncSeconds);
+
+    // LED indication that all is well
+    //digitalWrite(LED_BUILTIN,LOW);  // led de indicacion de que todo esta bien. 
+
+    return epoch;
+  }
+
+  // Failed to get an NTP/UDP response
+  Serial1.println("No response");
+  setSyncInterval(_resyncErrorSeconds);
+
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+// send an NTP request to the time server at the given address
+//-----------------------------------------------------------------------------
+void sendNTPpacket(const char* address) {
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+
+  // all NTP fields have been given values, now you can send a packet requesting a timestamp:
+  // Note that Udp.begin will request automatic translation (via a DNS server) from a
+  // name (eg pool.ntp.org) to an IP address. Never use a specific IP address yourself,
+  // let the DNS give back a random server IP address
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+
+  // Get the data back
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+
+  // All done, the underlying buffer is now updated
+  Udp.endPacket();
+}
+
+// -----------------------------------------------------------------------
+// Establish a WiFi connection with your router
+// -----------------------------------------------------------------------
+void connectToWifi() {
+  Serial1.print("Connecting to: ");
+  Serial1.println(_ssid);
+  WiFi.begin(_ssid, _password);
+
+  // Try to connect 4 times a second for X seconds before timing out
+  int timeout = WifiTimeOutSeconds * 4;
+  while (WiFi.status() != WL_CONNECTED && (timeout-- > 0)) {
+    delay(250);
+    Serial1.print(".");
+  }
+
+  // Successful connection?
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial1.println("\nFailed to connect, exiting");
+    // Set some LED failure here, for example
+    delay(1000);
+    return;
+  }
+
+  Serial1.print("\nWiFi connected with (local) IP address of: ");
+  Serial1.println(WiFi.localIP());
+}
+
+
+
+
+/*void setup_wifi() {
+  //luego reemplazar esta funcion por otra que utiliza la libreria wifi-multi.h
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial1.println();
+  Serial1.print("Connecting to ");
+  Serial1.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial1.print(".");
+  }
+
+  randomSeed(micros());//genera un numero aletaorio que parte del parametro micros() que devuelve el tiempo(hora:mm:ss:mls) en el que inicio el programa en la placa wemos-d1-mini
+
+  Serial1.println("");
+  Serial1.println("WiFi connected");
+  Serial1.println("IP address: ");
+  Serial1.println(WiFi.localIP());
+}*/
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  //El WemosD1mini posee esta logica para estar atento si hay nuevas publicaciones en el topic suscrito (casa/living/luz) y en caso afirmativo
+  //enviar un frame al Xbee-Router para que activar/desactivar Relee(foco)
+  
+  // Switch on the LED if an 1 was received as first character//envia un frame al XBee-Router(XBee-terminal) con "1" para activar el Relee que enciende el foco de luz
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is active low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial1.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ClienteWemosD1Mini-";
+    clientId += String(random(0xffff), HEX);//cada vez que nos reconectamos, debemos hacerlo con un nuevo clientID (no el mismo).
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) { //c_str() Convierte el contenido de una cadena a una cadena de estilo C. Nunca debe modificar la cadena a través del puntero devuelto.
+      Serial1.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("Log", "Dispositivo ESP8266ClientHome reconectado al servidor mqtt://mqtt.diveriot.com:1883");
+      // ... and resubscribe
+      client.subscribe("Casa/LivingRoom/Luz"); // Esto creo que esta de más? O me debo suscribir a tal TOPIC cuando me conecto por primera vez al serverMQTT?
+    } else {
+      Serial1.print("failed, rc=");
+      Serial1.print(client.state());
+      Serial1.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
 }
